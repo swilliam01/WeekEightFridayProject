9987d3937ac7c6d3957d36231fb6ac9782f92879
meli1010 <romielisse@gmail.com>
2018-07-20 14:02:26 -0400

    adding screenshot

--- fido.sql
+++ fido.sql
@@ -3,6 +3,8 @@ use fido;
 
 select * from user;
 
+select * from pet;
+
 -- get list of pets associated with user
 select a.id as petID, user_id, username, a.name, date_lost, description, status, image
 from pet a inner join user b on a.user_id = b.id; 
